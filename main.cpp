7a46d0c73935e2e4f307dd42cbd1bb726b5ba1c0
Дмитрий <luckybattle@yandex.ru>
2019-04-24 20:25:16 +0400

    first commit

--- main.cpp
+++ main.cpp
@@ -9,15 +9,5 @@ int main(int argc, char *argv[])
     qDebug()<<"SSL version use for run-time: "<<QSslSocket::sslLibraryVersionNumber();
     qDebug()<<QCoreApplication::libraryPaths();*/
 
-    QDomDocument tree;
-
-    //bool tmp = downloadHTML("file:///C:/Users/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9/Desktop/html_example.html", "test.html");
-    //bool tmp_1 = htmlToXml("test.html", "test.xml");
-    bool tmp_2 = parsingXml("test.xml", tree);
-
-    repDuplicateTags(tree);
-
-
-
     return a.exec();
 }
